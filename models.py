3ab6a7078664bd153107b50d1a5a74a956336b85
Jonathan Wong <jonathan.a.wong@gmail.com>
2020-07-29 15:28:08 -0700

    Created the user model and tables

--- models.py
+++ models.py
@@ -1,1 +1,29 @@
-"""Models for Blogly."""
+"""Demo file showing off a model for SQLAlchemy."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    """User."""
+
+    __tablename__="users"
+
+    id = db.Column(db.Integer, 
+        primary_key=True, 
+        autoincrement=True)
+
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+
+    image_url = db.Column(db.String(100), 
+        nullable=True, 
+        default="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png")
+
+   
