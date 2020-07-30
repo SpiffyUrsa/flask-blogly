28cbdcfcd7f8356eb58e4b880df08ce2bae61d0e
Jonathan Wong <jonathan.a.wong@gmail.com>
2020-07-29 17:34:04 -0700

    added database and form connectivity and functionality

--- app.py
+++ app.py
@@ -30,3 +30,60 @@ def users_listing():
     users_from_db = User.query.all()
 
     return render_template('/user_listing.html', users = users_from_db,)
+
+@app.route("/users/new")
+def show_new_user_form():
+    """ Shows an add new user form """
+    return render_template("new_user_form.html")
+
+@app.route("/users/new", methods=['POST'])
+def handle_new_user():
+    """ Handle the submission of the new user form """
+    first_name = request.form['first-name']
+    last_name = request.form['last-name']
+    image_url = request.form['image-url']
+
+    new_user = User(first_name = first_name, last_name = last_name, image_url = image_url)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/users")
+
+@app.route("/users/<user_id>")
+def display_user_info(user_id):
+    """ Shows information on user """
+    user = User.query.get_or_404(user_id)
+    return render_template("user_detail.html", user = user)
+
+@app.route("/users/<user_id>/edit")
+def display_edit_user_form(user_id):
+    """ Shows form for editing user info """
+    user = User.query.get_or_404(user_id)
+    return render_template("edit_user_form.html", user = user)
+
+@app.route("/users/<user_id>/edit", methods=['POST'])
+def handle_edit_user(user_id):
+    """ Handle the submission of the edit user form """
+    user = User.query.get(user_id)
+    first_name = request.form['first-name']
+    last_name = request.form['last-name']
+    image_url = request.form['image-url']
+
+    user.first_name = first_name
+    user.last_name = last_name
+    user.image_url = image_url 
+
+    db.session.commit()
+
+    return redirect("/users")
+
+@app.route("/users/<user_id>/delete", methods=['POST'])
+def handle_delete_user(user_id):
+    """ Handle the deleting of a user """
+    user_id = request.form["delete-user"]
+    user = User.query.get(user_id)
+    
+    db.session.delete(user)
+    db.session.commit()
+
+    return redirect("/users")
